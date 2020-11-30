fcb5c42dc4cb489be3d6cffd0ac021fdbeb6b875
carlos mur <cmurestudillos@gmail.com>
2020-11-30 07:46:11 +0100

    Componente de Error

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 // Componentes
 import HomeComponent from '../components/home/HomeComponent.vue';
+import BuscarComponent from '../components/buscar/BuscarComponent.vue';
+import PeliculaComponent from '../components/pelicula/PeliculaComponent.vue';
 import ErrorComponent from '../components/shared/error/ErrorComponent.vue';
 
 // ---------------------------------------------------------------//
@@ -13,6 +15,10 @@ Vue.use(VueRouter);
 const routes = [
     {path: '/', redirect: '/home'},
     {path: '/home', component: HomeComponent},
+    {path: 'buscar', component: BuscarComponent},
+    {path: 'buscar/:texto', component: BuscarComponent},
+    {path: 'pelicula/:id/:pag', component: PeliculaComponent},
+    {path: 'pelicula/:id/:pag/:busqueda', component: PeliculaComponent},
     {path: '*', component: ErrorComponent }
 ];
 

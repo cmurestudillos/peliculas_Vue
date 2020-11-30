20cb311ba69336038630d11f62fa77944b9babb3
carlos mur <cmurestudillos@gmail.com>
2020-11-30 08:13:46 +0100

    Creacion y estilos: FooterComponent

--- src/components/shared/footer/FooterComponent.vue
+++ src/components/shared/footer/FooterComponent.vue
@@ -1,5 +1,16 @@
 <template>
-    <p>FooterComponent</p>  
+  <!-- Footer -->
+  <footer class="fixed-bottom bg-custom">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center text-white">
+      <i>Copyright © - Carlos Mur</i>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
+
 </template>
 
 <script>
